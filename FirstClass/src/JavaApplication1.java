9b6f3a8a2932857d7ef4165fb3e34cd568f13a2a
Gustavo Roos <gustavohroos@gmail.com>
2021-08-30 15:46:35 -0300

    training

---
+++ FirstClass/src/JavaApplication1.java
@@ -0,0 +1,7 @@
+import java.lang.Math;
+public class JavaApplication1 {
+    public static void main(String[] args) throws Exception {
+        System.out.printf("Raiz de 49: %.2f\n", Math.sqrt(49.0));
+        System.out.printf("Potência 2^3: %.2f\n", Math.pow(2,3));
+    }
+}
